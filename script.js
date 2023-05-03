22c6973fbaf18df3a2f45df1f54d79b2a1e6825b
Aniss <anissnahi@gmail.com>
2023-05-03 11:41:41 -0400

    push

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 function calculateTotal() {
   const hours = parseFloat(document.getElementById("hours").value);
   const rate = parseFloat(document.getElementById("rate").value);
-  const errorsElement = document.getElementById("errors");
-  const resultElement = document.getElementById("result");
-  
-  if (!Number.isFinite(hours) || !Number.isFinite(rate)) {
-    errorsElement.textContent = "Veuillez entrer des nombres valides.";
-    resultElement.innerHTML = "";
+  if (isNaN(hours) || isNaN(rate) || hours < 0 || rate < 0) {
+    document.getElementById("errors").innerHTML = "Veuillez entrer des nombres valides et positifs.";
+    document.getElementById("result").innerHTML = "";
   } else {
     const total = hours * rate;
     const tpsRate = 0.05;
@@ -14,9 +11,8 @@ function calculateTotal() {
     const tvqRate = 0.09975;
     const tvq = total * tvqRate;
     const finalTotal = total + tps + tvq;
-    
-    errorsElement.textContent = "";
-    resultElement.innerHTML = `
+    document.getElementById("errors").innerHTML = "";
+    document.getElementById("result").innerHTML = `
       <p>Taux horaire: $${rate.toLocaleString('fr-CA', {minimumFractionDigits: 2, maximumFractionDigits: 2})}</p>
       <p>Nombre d'heures travaillées: ${hours.toLocaleString('fr-CA')}h</p>
       <h2>Total: $${total.toLocaleString('fr-CA', {minimumFractionDigits: 2, maximumFractionDigits: 2})}</h2>
@@ -26,6 +22,7 @@ function calculateTotal() {
     `;
   }
 }
+
 
 
 function clearForm() {
